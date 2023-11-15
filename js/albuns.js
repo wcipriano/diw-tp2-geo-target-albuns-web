2b832d07fb8dee58a5d451c8f84c42d1fc3e686c
wcipriano <wagner@solucionarti.com.br>
2023-11-15 17:54:00 -0300

    :sparkles: get albums content dynamically

---
+++ js/albuns.js
@@ -0,0 +1,37 @@
+function build_card(album) {
+  const html = `
+  <div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xxl-2">
+    <a class="text-decoration-none text-reset" href="./detalhe_album.html?${album.id}">
+      <div class="card">
+        <img src="${album.cover}" class="card-img-top" alt="${album.name}">
+        <div class="card-body">
+          <h5 class="card-title text-truncate">${album.name}</h5>
+          <p class="card-text line-clamp-3">${album.description}</p>
+        </div>
+      </div>
+    </a>
+  </div>
+  `;
+  return html;
+}
+
+function get_albuns() {
+  const url = "https://api-albuns.wagnercipriano.repl.co/albums";
+  fetch(url)
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      let html_cards = "";
+      json.forEach((item) => {
+        console.log(item);
+        html_cards += build_card(item);
+      });
+      let ac = document.getElementById("albun-container");
+      ac.innerHTML = html_cards;
+    });
+}
+
+window.addEventListener("load", (ev) => {
+  get_albuns();
+});
