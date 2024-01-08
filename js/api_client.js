d36016b7d56752c9b8d2b1d5dc4966eb23e499ef
wcipriano <wagner@solucionarti.com.br>
2024-01-08 18:31:49 -0300

    :ambulance: change API URL (replit)

--- js/api_client.js
+++ js/api_client.js
@@ -1,4 +1,6 @@
-const urlBase = "https://api-albuns.wagnercipriano.repl.co";
+// const urlBase = "https://api-albuns.wagnercipriano.repl.co";
+const urlBase =
+  "https://233a12c3-e495-429a-b932-f7b05fdb747b-00-2ij8stygk0xz0.spock.replit.dev";
 
 async function get_albums() {
   const url = `${urlBase}/albums`;
